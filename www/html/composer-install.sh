75e51e3cc34379f41eae62d784d3b52ce2a81da5
Bernhard Enders <bgeneto@users.noreply.github.com>
2024-02-12 16:34:13 -0300

    Update composer-install.sh

--- www/html/composer-install.sh
+++ www/html/composer-install.sh
@@ -1,1 +1,6 @@
+#!/usr/bin/sh
 
+composer create-project codeigniter4/appstarter codeigniter
+composer create-project symfony/skeleton:"7.0.*" symfony
+composer create-project laravel/laravel:"11.x-dev" laravel
+ 
