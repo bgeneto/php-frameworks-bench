860d287899a255e06b5146610a46a9cb973c66bf
Bernhard Enders <bgeneto@users.noreply.github.com>
2024-02-13 15:38:20 -0300

    update permissions

--- build.sh
+++ build.sh
@@ -7,20 +7,18 @@ docker compose build --parallel
 docker compose up -d
 
 # enter php docker container to install all required frameworks via composer
-docker exec -it -u 33 php_fpm_fmbench composer create-project codeigniter4/appstarter codeigniter
-docker exec -it -u 33 php_fpm_fmbench composer create-project symfony/skeleton:"7.0.*" symfony
-docker exec -it -u 33 php_fpm_fmbench composer require symfony/twig-bundle
-docker exec -it -u 33 php_fpm_fmbench composer create-project laravel/laravel:"11.x-dev" laravel
-docker exec -it -u 33 php_fpm_fmbench composer create-project pocketarc/codeigniter codeigniter3
+docker exec -it php_fpm_fmbench composer create-project codeigniter4/appstarter codeigniter
+docker exec -it php_fpm_fmbench composer create-project symfony/skeleton:"7.0.*" symfony
+docker exec -it php_fpm_fmbench composer require symfony/twig-bundle
+docker exec -it php_fpm_fmbench composer create-project laravel/laravel:"11.x-dev" laravel
+docker exec -it php_fpm_fmbench composer create-project pocketarc/codeigniter codeigniter3
 
 # copy our controllers, views, routes to each framework
 cp -r ./src/* ./www/html/
 
 # set the right permissions to www-data
-sudo chown -R 33:1000 ./src/
+echo -e "We need 'sudo' to set the proper ownership in 'www' folder to 'www-data' user:\n"
+sudo chown -R 33:$UID ./www
 
 # restart all containers (just in case)
 docker compose down && docker compose up -d
-
-
-
