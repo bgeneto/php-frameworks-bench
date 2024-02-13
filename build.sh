3db4db460c54f29a9dc2074620745904ebf6763a
Bernhard Enders <bgeneto@users.noreply.github.com>
2024-02-13 16:33:32 -0300

    new ci3 controller

--- build.sh
+++ build.sh
@@ -9,16 +9,18 @@ docker compose up -d
 # enter php docker container to install all required frameworks via composer
 docker exec -it php_fpm_fmbench composer create-project codeigniter4/appstarter codeigniter
 docker exec -it php_fpm_fmbench composer create-project symfony/skeleton:"7.0.*" symfony
-docker exec -it php_fpm_fmbench composer require symfony/twig-bundle
+docker exec -it php_fpm_fmbench bash -c 'cd symfony; composer require symfony/twig-bundle'
 docker exec -it php_fpm_fmbench composer create-project laravel/laravel:"11.x-dev" laravel
+docker exec -it php_fpm_fmbench bash -c 'cd laravel; php artisan route:clear'
 docker exec -it php_fpm_fmbench composer create-project pocketarc/codeigniter codeigniter3
 
-# copy our controllers, views, routes to each framework
-cp -r ./src/* ./www/html/
+# save current user id
+userid=`echo $UID`
 
 # set the right permissions to www-data
-echo -e "We need 'sudo' to set the proper ownership in 'www' folder to 'www-data' user:\n"
-userid=`echo $UID`
+echo -e "We need 'sudo' to copy files and set the proper ownership:\n"
+
+sudo cp -r ./src/* ./www/html/
 sudo chown -R 33:$userid ./www
 
 # restart all containers (just in case)
