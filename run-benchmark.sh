cbc070599685a9c33520cb6203f37674ffccf008
Bernhard Enders <bgeneto@users.noreply.github.com>
2024-02-19 23:15:04 -0300

    running docker with UID

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -96,4 +96,4 @@ for test in $benchmarks; do
 done
 
 # plot the results to html file
-docker run --rm -v ./:/usr/src/app bench/python3 /usr/src/app/plot.py
+docker run -u $UID --rm -v ./:/usr/src/app bench/python3 /usr/src/app/plot.py
